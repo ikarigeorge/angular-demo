48a0b231c97ada74aa0a5fa2ff6ebe09a5173b8d
ikarigeorge <ikarigeorge@gmail.com>
2017-08-21 15:05:33 +0000

    adding models and first view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ApiService } from './core/api.service';
+import { LoadingComponent } from './core/loading.component';
+import { DatePipe } from '@angular/common';
+import { UtilsService } from './core/utils.service';
+import { FilterSortService } from './core/filter-sort.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [Title],
+  providers: [Title, 
+    ApiService,
+    DatePipe,
+    UtilsService,
+    FilterSortService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/api.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/catch';
+import { ENV } from './env.conf';
+import { EventModel } from './models/event.model';
+import { RsvpModel } from './models/rsvp.model';
+
+@Injectable()
+export class ApiService {
+
+  constructor(
+    private http: HttpClient) { }
+
+  // GET list of public, future events
+  getEvents$(): Observable<EventModel[]> {
+    return this.http
+      .get(`${ENV.BASE_API}events`)
+      .catch(this._handleError);
+  }
+
+  // GET all events - private and public (admin only)
+  getAdminEvents$(): Observable<EventModel[]> {
+    return this.http
+      .get(`${ENV.BASE_API}events/admin`)
+      .catch(this._handleError);
+  }
+
+  // GET an event by ID (login required)
+  getEventById$(id: string): Observable<EventModel> {
+    return this.http
+      .get(`${ENV.BASE_API}event/${id}`)
+      .catch(this._handleError);
+  }
+
+  // GET RSVPs by event ID (login required)
+  getRsvpsByEventId$(eventId: string): Observable<RsvpModel[]> {
+    return this.http
+      .get(`${ENV.BASE_API}event/${eventId}/rsvps`)
+      .catch(this._handleError);
+  }
+
+  private _handleError(err: HttpErrorResponse | any) {
+    const errorMsg = err.message || 'Error: Unable to complete request.';
+    return Observable.throw(errorMsg);
+  }
+
+}
